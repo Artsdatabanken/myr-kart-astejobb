d54576dfad5a2eb4c04dee84df0bdd00fbbcd55a
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-11-09 01:12:16 +0100

    Rename data -> temp

--- stages/transform/10_geojson.sh
+++ stages/transform/10_geojson.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -e
-ogr2ogr -oo GEOM_POSSIBLE_NAMES=geom -oo KEEP_GEOM_COLUMNS=NO myr.geojson myr.csv
+ogr2ogr -oo GEOM_POSSIBLE_NAMES=geom -oo KEEP_GEOM_COLUMNS=NO temp/myr.geojson temp/myr.csv
