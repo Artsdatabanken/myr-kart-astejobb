4c0b4d5bea7b84443ccd23733895cbf50f27ef73
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-11-09 01:00:27 +0100

    Kosmetikk geojson-fil

--- stages/transform/20_map.js
+++ stages/transform/20_map.js
@@ -2,49 +2,60 @@ const { io, json } = require("lastejobb");
 
 const statistics = {};
 
+const arraysInDisguise = [
+  "Tormarksform",
+  "versjoner",
+  "Fremstad",
+  "Vannlig veg",
+  "Sjelden veg",
+  "myr struktur",
+  "Naturtype"
+];
 const src = io.lesDatafil("myr.geojson");
 src.features.forEach(feature => {
   src.name = "polygons";
   const props = feature.properties;
+  addStatistics(props);
+  for (var arraykey of arraysInDisguise) toArray(props, arraykey);
   delete props.rowNumber;
   delete props.locality;
   delete props.kommnr;
   delete props.areal_m2;
+  json.moveKey(props, "Fremstad", "fremstad");
   json.moveKey(props, "myr_id", "autorkode");
   json.moveKey(props, "myr struktur", "myrmassivstruktur");
+  json.moveKey(props, "Vannlig veg", "vegetasjon.vanlig");
+  json.moveKey(props, "Sjelden veg", "vegetasjon.sjelden");
 
   props.koder = [
     ...mapNaturtype(props.Naturtype),
     ...mapLandform(props.Tormarksform)
   ];
-  addStatistics(props);
+  delete props.Naturtype;
+  delete props.Tormarksform;
 });
 
-io.skrivDatafil("myr.4326.geojson", src);
+io.skrivBuildfil("myr.4326.geojson", src);
 io.skrivDatafil("stats", statistics);
 
 function mapNaturtype(na) {
   const r = [];
-  na.replace(/\//g, "|")
-    .split("|")
-    .forEach(t => {
-      if (t === "-" || t.length <= 0) return;
-      t = t.replace("L4-C", "L4");
-      r.push("NN-NA-TI-" + t);
-    });
+  (na || []).forEach(t => {
+    if (t === "-" || t.length <= 0) return;
+    t = t.replace("L4-C", "L4");
+    r.push("NN-NA-TI-" + t);
+  });
   return new Set(r);
 }
 
 function mapLandform(tf) {
   const r = [];
-  tf.replace(/\//g, "|")
-    .split("|")
-    .forEach(t => {
-      t = t.replace("3TO-", "");
-      if (t === "XX" || t === "-" || t === "0" || t.length <= 0)
-        return "NN-NA-BS-3TO";
-      r.push("NN-NA-BS-3TO-" + t);
-    });
+  (tf || []).forEach(t => {
+    t = t.replace("3TO-", "");
+    if (t === "XX" || t === "-" || t === "0" || t.length <= 0)
+      return "NN-NA-BS-3TO";
+    r.push("NN-NA-BS-3TO-" + t);
+  });
   return new Set(r);
 }
 
@@ -61,4 +72,10 @@ function addStatistics(props) {
       }
     }
   }
+}
+
+function toArray(props, key) {
+  if (!props[key]) return [];
+  const arr = props[key].replace("/", "|").split("|");
+  props[key] = [...new Set(arr)];
 }
